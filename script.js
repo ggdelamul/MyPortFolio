62779e5023ef46c2e074a1abb2943825e508a6ba
jeremy <projeremylegendre@gmail.com>
2022-11-25 19:02:21 +0100

    added style link effect on navbar-mobile-menu

--- script.js
+++ script.js
@@ -1,11 +1,5 @@
-console.log("connecté");
-//je sélectionne et je stocke
 const icone = document.querySelector("i");
-console.log(icone);
 const modale = document.querySelector(".modal");
-console.log(modale);
-
-//j'aplique un évènement et une action
 icone.addEventListener("click", function () {
   console.log("icone cliqué");
   icone.classList.toggle("fa-times");
