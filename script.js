0d58d666826b2f13ddcbf4c993493df1aeceff9a
jeremy <projeremylegendre@gmail.com>
2022-11-25 18:14:40 +0100

    added script for menu scrolling

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
-console.log("connecté!");
-const burger = document.querySelector("i");
-console.log(burger);
-const modal = document.querySelector(".modal");
-console.log(modal);
+console.log("connecté");
+//je sélectionne et je stocke
+const icone = document.querySelector("i");
+console.log(icone);
+const modale = document.querySelector(".modal");
+console.log(modale);
+
+//j'aplique un évènement et une action
+icone.addEventListener("click", function () {
+  console.log("icone cliqué");
+  icone.classList.toggle("fa-times");
+  modale.classList.toggle("change-modal");
+});
