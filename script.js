c21ef0f48be456ea283e0183eaeabd62d32699bb
jeremy <projeremylegendre@gmail.com>
2023-01-04 19:47:13 +0100

    first commit js modale

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
+//Menu Mobile
 const icone = document.querySelector("i");
-const modale = document.querySelector(".modal");
+const menuModale = document.querySelector(".modal");
 icone.addEventListener("click", function () {
   console.log("icone cliqué");
   icone.classList.toggle("fa-times");
-  modale.classList.toggle("change-modal");
+  menuModale.classList.toggle("change-modal");
 });
+//Modale
+const knowMore = document.querySelectorAll(".knowMore");
+const cardModale = document.querySelectorAll(".modal-container");
+console.log(knowMore);
+console.log(cardModale);
+
+function showModal(index) {
+  cardModale[index].style.display = "block";
+}
