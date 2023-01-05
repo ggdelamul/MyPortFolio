6daa473d816f0879deddf938d4955f64352886b2
jeremy <projeremylegendre@gmail.com>
2023-01-05 19:05:45 +0100

    begin js for modale display none

--- script.js
+++ script.js
@@ -9,17 +9,26 @@ icone.addEventListener("click", function () {
 //Modale
 const knowMore = document.querySelectorAll(".knowMore");
 const cardModale = document.querySelectorAll(".modal-container");
+const closeCross = document.querySelectorAll(".modal-container i");
 console.log(knowMore);
 console.log(cardModale);
+console.log(closeCross);
 function showModal(index) {
   cardModale[index].style.display = "block";
 }
-function listenClick(indice) {
+function listenClickLink(indice) {
   knowMore[indice].addEventListener("click", function (e) {
     e.preventDefault();
     showModal(indice);
   });
 }
+function displayModal(index) {
+  cardModale[index].style.display = "none";
+}
+
 for (i = 0; i < knowMore.length; i++) {
-  listenClick(i);
+  listenClickLink(i);
+  closeCross[i].addEventListener("click", function () {
+    console.log("croix cliqué");
+  });
 }
